74dd4551e1fdea33deb4bdda4f0ad8f3f2fc59a4
Atharva Chougale <94557388+Godx00@users.noreply.github.com>
2023-12-06 21:46:43 +0530

    Update App.js

--- chatapp/client/client/src/App.js
+++ chatapp/client/client/src/App.js
@@ -2,13 +2,23 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Agents from './components/Agents';
 import Clients from './components/Clients';
+import Home from './components/Home';
+import UserLogin from './components/UserLogin';
+import AgentLogin from './components/AgentLogin';
+import Dashboard from './components/Dashboard';
+import Questionari from './components/Questionari';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='client' element={<Clients></Clients>} ></Route>
-        <Route path='agent' element={<Agents></Agents>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/userlogin" element={<UserLogin />} />
+        <Route path="/agentlogin" element={<AgentLogin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/questionari" element={<Questionari />} />
+        <Route path='/client' element={<Clients></Clients>} ></Route>
+        <Route path='/agent' element={<Agents></Agents>}></Route>
       </Routes>
     </div>
   );
